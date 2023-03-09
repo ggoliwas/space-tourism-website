44323424bfc0ec5046968fa0ccf88e0b6368cd99
Grzegorz Goliwąs <goliwas.grzegorz@gmail.com>
2023-03-09 22:33:28 +0100

    last fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,15 +13,15 @@ const Header = () => {
     return(
         <header className="primary-header flex">
             <div>
-                <img src={process.env.PUBLIC_URL+'/assets/shared/logo.svg'} className="logo"/>
+                <img src={process.env.PUBLIC_URL+'/assets/shared/logo.svg'} className="logo" alt="logo space tourism"/>
             </div>
             <button className="mobile-nav-toogle" aria-expanded={isToogle} aria-controls="primary-navigation" onClick={() => setToogle(!isToogle)}></button>
             <nav>
                 <ul id="primary-navigation" className="primary-navigation underline-indicators flex" data-visible={isToogle}>
-                    <NavLink to="/" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>00</span>home</NavLink>
-                    <NavLink to="destination" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>01</span>destinations</NavLink>
-                    <NavLink to="crew" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>02</span>crew</NavLink>
-                    <NavLink to="technology" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>03</span>technology</NavLink>
+                    <li><NavLink to="/" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>00</span>home</NavLink></li>
+                    <li><NavLink to="destination" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>01</span>destinations</NavLink></li>
+                    <li><NavLink to="crew" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>02</span>crew</NavLink></li>
+                    <li><NavLink to="technology" activeClassName="active" className="ff-sans-cond text-white uppercase letter-spacing-2"><span>03</span>technology</NavLink></li>
                 </ul>
             </nav>
         </header>
