e9708e7635d57e40caf2a9d71145180bef81957b
Grzegorz Goliwąs <goliwas.grzegorz@gmail.com>
2023-03-14 19:47:41 +0100

    add readme file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
                 <h1 className="ff-sans-cond fs-500 text-accent uppercase letter-spacing-3">so, you want travel to <span className="ff-serif text-white fs-900 d-block">space</span></h1>
                 <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
-            <Link to="destination" className="button button-large uppercase ff-serif text-dark">explore</Link>
+            <Link to="/destination" className="button button-large uppercase ff-serif text-dark">explore</Link>
         </main>
     )
 }
